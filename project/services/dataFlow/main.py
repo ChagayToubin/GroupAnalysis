ef878f91a213ff1377517977cd0a888fbc5ba4ff
chagay <chagaytoubin@gmail.com>
2025-09-16 23:00:34 +0300

    fisnih connect tcvi and chili to client

--- project/services/dataFlow/main.py
+++ project/services/dataFlow/main.py
@@ -1,6 +1,6 @@
-from mongoDB import DAL
+from project.services.dataFlow.mongoDB import DAL
 
-from group_type_analyzer import DataFlow
+from project.services.dataFlow.group_type_analyzer import DataFlow
 
 class Menger:
     def __init__(self):
@@ -9,9 +9,12 @@ class Menger:
 
     def menger(self):
         all_data = self.data.dal("https://t.me/+ztOtIXepdDVkYjE0")
-        self.classifier.data_classifier(all_data)
-
-if __name__ == "__main__":
-    start = Menger()
-    while True:
-        start.menger()
+        dic=self.classifier.data_classifier(all_data)
+        return dic
+#
+#
+# if __name__ == "__main__":
+#     start = Menger()
+#     while True:
+#         print("ds")
+#         start.menger()
