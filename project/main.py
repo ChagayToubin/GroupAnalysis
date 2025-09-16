c314268aa00aa8018aa5514f03f3702fd93388a2
chagay <chagaytoubin@gmail.com>
2025-09-16 10:04:16 +0300

    server get ready

--- project/main.py
+++
@@ -1,9 +0,0 @@
-from project.manager import ManagerServer
-import asyncio
-
-async def main():
-    manager = ManagerServer()
-    await manager.app()
-
-if __name__ == "__main__":
-    asyncio.run(main())
