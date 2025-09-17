6ced3179c36d60ca9ced964eeeace05c3a907350
chagay <chagaytoubin@gmail.com>
2025-09-17 11:08:56 +0300

    finsih check pull dataand dataflow

--- project/services/dataFlow/mongoDB.py
+++ project/services/dataFlow/mongoDB.py
@@ -6,11 +6,14 @@ class DAL:
         myclient = pymongo.MongoClient("mongodb+srv://db_test:123456TEST@cluster0.ziuim0x.mongodb.net/")
         mydb = myclient["telegram_data"]
         self.mycol = mydb["fs.files"]
+        self.classify_categories = ["neutral","teror","safe","suspicious"]
+        self.types = ["text","video","audio","image"]
 
-    def dal(self,skiper):
-        my_list = []
-        a = self.mycol.find({},{"metadata.category":1,"metadata.group_link":1,"metadata.classify":1,"_id":0}).skip(skiper)
-        for i in a:
-            my_list.append(i["metadata"])
-        return my_list
+    def dal(self,group_name):
+        my_dict = {}
+        for category in self.classify_categories:
+            my_dict[category] = {}
+            for type in self.types:
+                my_dict[category][type] = self.mycol.count_documents({"metadata.group_link":group_name,"metadata.category":type,"metadata.classify":category})
 
+        return my_dict
