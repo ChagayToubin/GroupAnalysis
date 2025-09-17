b5e5cd89217a724945b3e02c2fb1fb4979114bf9
chagay <chagaytoubin@gmail.com>
2025-09-17 11:04:53 +0300

    finsih checl pull data

--- project/services/pull_data/app/main.py
+++ project/services/pull_data/app/main.py
@@ -1,6 +1,6 @@
 from telegram_manager import TelegramManager
 
 manager = TelegramManager()
-group_link = "https://t.me/+ztOtIXepdDVkYjE0"
+group_link = "https://t.me/TheBigBadShadow"
 manager.run_monitor(group_link)
 
