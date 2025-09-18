027b85f2f240b7df225ae7d3c8a546d95da62088
Tzvi Zait <z0548430441@gmail.com>
2025-09-18 09:24:52 +0300

    I fixed the group classifier and also the main

--- project/services/dataFlow/mongoDB.py
+++ project/services/dataFlow/mongoDB.py
@@ -6,7 +6,7 @@ class DAL:
         myclient = pymongo.MongoClient("mongodb+srv://db_test:123456TEST@cluster0.ziuim0x.mongodb.net/")
         mydb = myclient["telegram_data"]
         self.mycol = mydb["fs.files"]
-        self.classify_categories = ["neutral","teror","safe","suspicious"]
+        self.classify_categories = ["Terrorism","financial","sexual","leisure"]
         self.types = ["text","video","audio","image"]
 
     def dal(self,group_name):
@@ -14,6 +14,6 @@ class DAL:
         for category in self.classify_categories:
             my_dict[category] = {}
             for type in self.types:
-                my_dict[category][type] = self.mycol.count_documents({"metadata.group_link":group_name,"metadata.category":type,"metadata.classify":category})
+                my_dict[category][type] = self.mycol.count_documents({"metadata.group_link":group_name,"metadata.category":type,"metadata.classification":category})
 
         return my_dict
